68861da207ce7cdc27b8a45167a813b36ab522a3
RobertOcici1 <robert.ocici@student.utamu.ac.ug>
2024-12-12 20:06:29 +0200

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,8 @@
+<?php
+ $host = 'localhost'; $user = 'root'; $pass =''; $door = 'notificationsystem'; 
+	$link=mysqli_connect($host,$National ID,$Username,$DOB,$Email,$Gender,$Address,$Occuptions ,$pass,$door);
+	if($link==false){
+		die("Error:can't connect".mysqli_connect_error());
+		}
+
+?>
